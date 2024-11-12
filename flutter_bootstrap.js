85faa50cd314ddf9a20b1aacc6cddf372436fe6b
joelwasike <joelwasike97@gmail.com>
2024-11-12 16:22:54 +0300

    first

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "451680461"
+    serviceWorkerVersion: "2074404121"
   }
 });
